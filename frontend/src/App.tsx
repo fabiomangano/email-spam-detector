2471e0d0cbad45758e4f2db75f1d65b420250eda
Fabio Mangano <37954832+fabiomangano@users.noreply.github.com>
2025-06-03 23:09:01 +0200

    wip: home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import "@mantine/core/styles.css";
+import '@mantine/dropzone/styles.css';
+
 import { MantineProvider } from "@mantine/core";
 import { Routes, Route } from "react-router";
 import { AppShell, Burger } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { Anchor } from "@mantine/core";
+import { Link } from "react-router";
 import Home from "./pages/Home";
-
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
@@ -25,7 +28,20 @@ function App() {
           <div>Logo</div>
         </AppShell.Header>
 
-        <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+        <AppShell.Navbar p="md">
+          <Anchor component={Link} to="/">
+            Home
+          </Anchor>
+          <Anchor component={Link} to="/report">
+            Report
+          </Anchor>
+          <Anchor component={Link} to="/report">
+            Config
+          </Anchor>
+           <Anchor component={Link} to="/guide">
+            Guide
+          </Anchor>
+        </AppShell.Navbar>
 
         <AppShell.Main>
           <Routes>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,91 @@
+import { Tabs } from "@mantine/core";
+import {
+  IconMessageCircle,
+  IconSettings,
+} from "@tabler/icons-react";
+import { Textarea } from "@mantine/core";
+import { Group, Text } from "@mantine/core";
+import { IconUpload, IconPhoto, IconX } from "@tabler/icons-react";
+import { Dropzone,  IMAGE_MIME_TYPE } from "@mantine/dropzone";
+
 function Home() {
-    return <div> HOME </div>
+  return (
+    <Tabs variant="outline" defaultValue="gallery">
+      <Tabs.List>
+        <Tabs.Tab value="gallery" leftSection={<IconPhoto size={12} />}>
+          Text
+        </Tabs.Tab>
+        <Tabs.Tab
+          value="messages"
+          leftSection={<IconMessageCircle size={12} />}
+        >
+          File
+        </Tabs.Tab>
+        <Tabs.Tab value="settings" leftSection={<IconSettings size={12} />}>
+          Data
+        </Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value="gallery">
+        <Textarea
+          label="Email text"
+          placeholder="Input placeholder"
+          autosize
+          minRows={25}
+        />
+      </Tabs.Panel>
+
+      <Tabs.Panel value="messages">
+        <Dropzone
+          onDrop={(files) => console.log("accepted files", files)}
+          onReject={(files) => console.log("rejected files", files)}
+          maxSize={5 * 1024 ** 2}
+          accept={IMAGE_MIME_TYPE}
+        >
+          <Group
+            justify="center"
+            gap="xl"
+            mih={220}
+            style={{ pointerEvents: "none" }}
+          >
+            <Dropzone.Accept>
+              <IconUpload
+                size={52}
+                color="var(--mantine-color-blue-6)"
+                stroke={1.5}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX
+                size={52}
+                color="var(--mantine-color-red-6)"
+                stroke={1.5}
+              />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconPhoto
+                size={52}
+                color="var(--mantine-color-dimmed)"
+                stroke={1.5}
+              />
+            </Dropzone.Idle>
+
+            <div>
+              <Text size="xl" inline>
+                Drag images here or click to select files
+              </Text>
+              <Text size="sm" c="dimmed" inline mt={7}>
+                Attach as many files as you like, each file should not exceed
+                5mb
+              </Text>
+            </div>
+          </Group>
+        </Dropzone>
+      </Tabs.Panel>
+
+      <Tabs.Panel value="settings">Settings tab content</Tabs.Panel>
+    </Tabs>
+  );
 }
 
 export default Home;
