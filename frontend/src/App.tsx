eb7a764a9c8703edc88468031a4abcc943671cde
Fabio Mangano <37954832+fabiomangano@users.noreply.github.com>
2025-06-03 21:11:36 +0200

    create basic home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { MantineProvider } from "@mantine/core";
 import { Routes, Route } from "react-router";
 import "@mantine/core/styles.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <MantineProvider>
       <Routes>
-        <Route index element={<>HOME</>} />
-         <Route path="report" element={<>REPORT</>} />
+        <Route index element={<Home/>} />
+        <Route path="report" element={<>REPORT</>} />
       </Routes>
     </MantineProvider>
   );
